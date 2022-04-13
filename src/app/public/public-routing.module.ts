835c4db6544ce796f7ba78f38d8412523f2f89f2
Ansaroll <andrianiainarolland123@gmail.com>
2022-04-13 14:41:04 +0300

    squellette public 1.2

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule , Routes } from "@angular/router";
-import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
     {
-        path : '' , component: HeaderComponent
+        path : '' , component: HomeComponent
     }
 ]
 
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { PublicRoutingModule } from './public-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
@@ -15,7 +14,6 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     LandingPageComponent,
     NavbarComponent,
-    HeaderComponent,
     CoursesComponent,
     ContactComponent,
     AboutComponent,
