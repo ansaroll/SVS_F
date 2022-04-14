59a97a88eaccb3c09cfc88a11880e778d39e5360
Ansaroll <andrianiainarolland123@gmail.com>
2022-04-14 16:10:39 +0300

    public/apropos1.2

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule , Routes } from "@angular/router";
 import { AboutComponent } from "./components/about/about.component";
 import { BodyComponent } from "./components/body/body.component";
-import { ContactComponent } from "./components/contact/contact.component";
+// import { ContactComponent } from "./components/contact/contact.component";
 import { CoursesComponent } from "./components/courses/courses.component";
 import { HomeComponent } from "./components/home/home.component";
 
@@ -14,7 +14,7 @@ const routes: Routes = [
           { path: 'acceuil' , component: HomeComponent },
           { path: 'about' , component: AboutComponent },
           { path: 'courses' , component: CoursesComponent },
-          { path: 'contact' , component: ContactComponent },
+          // { path: 'contact' , component: ContactComponent },
         ]
     }
 
