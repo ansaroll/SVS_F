483fb8721bfd9a052da8f67f415a1136882bc0d8
Ansaroll <andrianiainarolland123@gmail.com>
2022-10-07 23:09:29 +0300

    getAll Courses

--- src/app/admin/components/courses/courses.component.ts
+++ src/app/admin/components/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from 'src/app/core/services/courses.service';
+import { Courses } from 'src/app/models/courses.model';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses:Partial<Courses>[] = []
+
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
+    this.coursesService.getCoursess().subscribe({
+      next:data => {
+        this.courses = data
+      },
+      error:err => console.log({err})
+    })
   }
 
 }
